cd9a2f622c622d07279b6c251b130485846d9690
shenliyang <tsl-shenliyang@tasly.com>
2017-09-14 17:29:22 +0800

    travis 68

--- _travis.sh
+++ _travis.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#定义时间
 time=`date +%Y-%m-%d\ %H:%M:%S`
 
 function success(){
@@ -10,12 +11,30 @@ function failure(){
    echo "failure"
 }
 function default(){
-   echo "default"
-cat <<EOF >> README.md 
 
+  git clone https://${GH_REF} .deploy_git
+  cd .deploy_git
+  git checkout master
+
+  cd ../
+  mv .deploy_git/.git/ ./public/
+
+  cd ./public
+
+cat <<EOF >> README.md 
+Site updated time: $time 
+Site updated state: $TRAVIS_TEST_RESULT
+     
 EOF
 
-   echo $time
+  cat README.md
+
+  git init
+  git config user.name "shenliyang"
+  git config user.email ""
+  git add .
+  git commit -m "Build by Travis CI"
+  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master
 } 
 
 
@@ -26,6 +45,7 @@ case $1 in
     "failure")
 	     failure
 	     ;;
-	  *) default	
+	         *) 
+       default	
 esac
 
