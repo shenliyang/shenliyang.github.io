be8412dd0433ce813e503392d4559d9449523887
tsl-shenliyang <tsl-shenliyang@tasly.com>
2018-07-23 17:35:17 +0800

    修改同步部署

--- _travis.sh
+++ _travis.sh
@@ -49,9 +49,17 @@ EOF
   git config user.email ""
   git add .
   git commit -m "Update Blog By TravisCI With Build $TRAVIS_BUILD_NUMBER"
+  # Github Pages
   git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master
+  # Gitee Pages
   git push --force --quiet "https://shenliyang:${GITEE_TOKEN}@${GITEE_REF}" master:master
 
+  # Create Tag
+  git tag v1.2.$TRAVIS_BUILD_NUMBER -a -m "Auto Taged By TravisCI With Build $TRAVIS_BUILD_NUMBER"
+  # Github Pages
+  - git push --quiet "https://${GH_TOKEN}@${GH_REF}" master:master --tags
+  # Gitee Pages
+  - git push --quiet "https://shenliyang:${GITEE_TOKEN}@${GITEE_REF}" master:master --tags
 }
 
 case $1 in
